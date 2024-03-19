68742421e24310b1291cd07a396b5a049466e3a7
Rahul G <135408406+itsrahulg@users.noreply.github.com>
2024-03-19 17:41:59 +0530

    Initial commit

---
+++ src/main/java/com/c2c/dynamo/DynamoDBConfig.java
@@ -0,0 +1,51 @@
+package com.c2c.dynamo;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class DynamoDBConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDB() {
+        if(environment.getActiveProfiles()[0].equals("local")){
+            return AmazonDynamoDBClientBuilder.standard()
+                    .withCredentials(amazonAWSCredentialsProvider())
+                    .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4567", "ap-south-1"))
+                    .build();
+        }
+        else if (environment.getActiveProfiles()[0].equals("dev"))
+            return AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(amazonAWSCredentialsProvider())
+                .withRegion(Regions.AP_SOUTH_1)
+                .build();
+        else
+            return AmazonDynamoDBClientBuilder.standard().build();
+    }
+
+    /*@Bean
+    public AWSCredentials amazonAWSCredentials() {
+        // Or use an AWSCredentialsProvider/AWSCredentialsProviderChain
+        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+    }*/
+
+    public AWSCredentialsProvider amazonAWSCredentialsProvider() {
+        return new ProfileCredentialsProvider();
+    }
+
+}
